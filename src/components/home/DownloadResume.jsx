cea1923a2b092bba5630cfa8a29fe35f25f2a0d8
SUJAY GOWDA <sujay.gowda@i4sight.net>
2024-04-22 13:12:13 +0530

    add: netlify.toml

--- src/components/home/DownloadResume.jsx
+++ src/components/home/DownloadResume.jsx
@@ -15,7 +15,7 @@ export default function DownloadResume() {
       <Link
         to="/public/resume/sujaygowda.pdf"
         target="_blank"
-        className=" bg-backgroundpx-2 absolute right-[50%] flex translate-x-[120%] translate-y-[-50%] items-center gap-2 py-1 font-vietnam text-xs capitalize text-text-secondary transition-colors duration-300 hover:text-accent xs:translate-x-[158%] md:translate-x-[182%] md:translate-y-[-90%] md:text-sm"
+        className=" absolute right-[50%] flex translate-x-[120%] translate-y-[-50%] items-center gap-2 bg-background px-2 py-1 font-vietnam text-xs capitalize text-text-secondary transition-colors duration-300 hover:text-accent xs:translate-x-[158%] md:translate-x-[182%] md:translate-y-[-90%] md:text-sm"
       >
         download resume
       </Link>
