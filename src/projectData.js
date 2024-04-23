17c1efc82aac6a717dec70f81c7e762fda5e7c9d
SUJAY GOWDA <sujay.gowda@i4sight.net>
2024-04-23 10:56:45 +0530

    update: project

--- src/projectData.js
+++ src/projectData.js
@@ -1,52 +1,52 @@
-import frontendFusion from './assets/projectCover/frontend-fusion.png';
-import cheffest from './assets/projectCover/cheffest.png';
-import towerBuddy from './assets/projectCover/towerbuddy.png';
-import i4sight from './assets/projectCover/i4sight.png';
+import frontendFusion from "./assets/projectCover/frontend-fusion.png";
+import cheffest from "./assets/projectCover/cheffest.png";
+import towerBuddy from "./assets/projectCover/towerbuddy.png";
+import i4sight from "./assets/projectCover/i4sight.png";
 
 export const projects = [
   {
-    name: 'cheffest',
+    name: "cheffest",
     image: cheffest,
-    projectLink: 'https://cheffest.netlify.app/',
-    githubLink: 'https://github.com/SujayGowdaa/cheffest',
+    projectLink: "https://cheffest.netlify.app/",
+    githubLink: "https://github.com/SujayGowdaa/cheffest",
     description:
-      'Cheffest is a food ordering app where users can order for food. The remote state of the application is managed with react query and the form validation using react hook form. The backend is powered by supabase.',
+      "Cheffest is a food ordering app where users can order for food. The remote state of the application is managed with react query and the form validation using react hook form. The backend is powered by supabase.",
     skills: [
-      'html',
-      'css',
-      'javaScript',
-      'react',
-      'react hook form',
-      'react query',
-      'tailwind css',
-      'supabase',
+      "html",
+      "css",
+      "javaScript",
+      "react",
+      "react hook form",
+      "react query",
+      "tailwind css",
+      "supabase",
     ],
   },
   {
-    name: 'frontend fusion',
+    name: "frontend fusion",
     image: frontendFusion,
-    projectLink: 'https://frontend-fusion.netlify.app/',
-    githubLink: 'https://github.com/SujayGowdaa/front-end-fusion',
+    projectLink: "https://frontend-fusion.netlify.app/",
+    githubLink: "https://github.com/SujayGowdaa/front-end-fusion",
     description:
-      'Frontend Fusion is a frontend application that functions essentially as a quiz app, allowing users to customise the topic and difficulty level to suit their interests. Redux is utilised to manage the application state.',
-    skills: ['html', 'css', 'javaScript', 'react', 'redux'],
+      "Frontend Fusion is a frontend application that functions essentially as a quiz app, allowing users to customise the topic and difficulty level to suit their interests. Redux is utilised to manage the application state.",
+    skills: ["html", "css", "javaScript", "react", "redux"],
   },
-  {
-    name: 'tower buddy',
-    image: towerBuddy,
-    projectLink: 'https://towerbuddy.tel/',
-    githubLink: '',
-    description:
-      'TowerBuddy provides the landowners a platform to list the available property, increasing the chances of the property being monetized at optimum rate.',
-    skills: ['html', 'css', 'javaScript', 'react', 'email JS'],
-  },
-  {
-    name: 'i4Sight',
-    image: i4sight,
-    projectLink: 'https://www.i4sight.net/',
-    githubLink: '',
-    description:
-      'Designed and developed my first static real-world project from scratch during my internship at the company. One of the first B2B2C platforms of its kind is i4Sight.',
-    skills: ['html', 'css', 'javaScript'],
-  },
+  // {
+  //   name: 'tower buddy',
+  //   image: towerBuddy,
+  //   projectLink: 'https://towerbuddy.tel/',
+  //   githubLink: '',
+  //   description:
+  //     'TowerBuddy provides the landowners a platform to list the available property, increasing the chances of the property being monetized at optimum rate.',
+  //   skills: ['html', 'css', 'javaScript', 'react', 'email JS'],
+  // },
+  // {
+  //   name: 'i4Sight',
+  //   image: i4sight,
+  //   projectLink: 'https://www.i4sight.net/',
+  //   githubLink: '',
+  //   description:
+  //     'Designed and developed my first static real-world project from scratch during my internship at the company. One of the first B2B2C platforms of its kind is i4Sight.',
+  //   skills: ['html', 'css', 'javaScript'],
+  // },
 ];
